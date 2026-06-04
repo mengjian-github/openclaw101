d322dba53cf1248471f4e3390caa687339786bdd
Kian Meng <xjtumj@gmail.com>
2026-06-04 18:30:09 +0000

    fix: add openclaw101 clarity tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -95,6 +95,14 @@ gtag('config', 'G-86ESEQC7V8');`}
 plausible.init=plausible.init||function(i){plausible.o=i||{}};
 plausible.init();`}
         </Script>
+
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "x1vov2nx6p");`}
+        </Script>
       </head>
       <body className="font-sans">{children}</body>
     </html>
